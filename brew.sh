b7840e3d48129b13cb10714777a7e42fd0f2f017
管子工具箱 <50480726+A7T@users.noreply.github.com>
2021-11-06 14:03:26 +0800

    Create brew.sh
    Install homebrew and use remote URLs of Homebrew repositories from environment.

---
+++ brew.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
+HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
