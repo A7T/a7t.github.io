a80a1d6991bc9f1fa4854ace27606fc68f39f2c7
管子工具箱 <50480726+A7T@users.noreply.github.com>
2021-11-06 14:07:37 +0800

    Update brew.sh
    Fix bugs.

--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
-HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
