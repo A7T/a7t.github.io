953c01367e9c14e88937f322824d2278c426d969
A7T <a7t@4rt.top>
2026-05-18 14:43:55 +0800

    Remove obsolete scripts

--- brew.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
-export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
